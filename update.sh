b2b12f66aa89d50dbb1178cc6269a89367d39ccd
Quentin Grosperrin <qgrosperrin@gdssecurity.com>
2015-11-21 22:43:12 +0000

    updating dotfiles

--- update.sh
+++ update.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env zsh 
 
 dotfiles="~/.zshrc ~/.zshuery ~/.bash_profile ~/.tmux.conf"
-rsync -avh --no-perms $(dotfiles) .
+cp -auR $dotfiles .
 
 git add --all
 git commit -am "updating dotfiles"
-git push origin master
+#git push origin master
