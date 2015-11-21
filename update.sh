fdff1b5637dad7b06a066c007205b37943a14428
Quentin Grosperrin <qgrosperrin@gdssecurity.com>
2015-11-21 22:23:44 +0000

    updating dotfiles

--- update.sh
+++ update.sh
@@ -1,4 +1,7 @@
-rsync -avh --no-perms --exclude "bootstrap.sh" --exclude "update.sh" `ls -a ~/.dotfiles` ~/.dotfiles
+#!/usr/bin/env zsh 
+
+local dotfiles=".zshrc .zshuery .bash_profile .bashrc .tmux.conf"
+rsync -avh --no-perms $(dotfiles) .
 
 git add *
 git commit -m "updating dotfiles"
