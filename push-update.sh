90b07a9e3b60170a8256c3c17cfcae07d8eee9e5
Quentin Grosperrin <qgrosperrin@gdssecurity.com>
2018-07-04 14:31:58 +0200

    updating dotfiles

--- push-update.sh
+++ push-update.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env zsh 
 
 cd $HOME/.dotfiles
-dotfiles="$HOME/.zshrc $HOME/.zshuery $HOME/.bash_profile $HOME/.tmux.conf $HOME/.vimrc"
+dotfiles="$HOME/.zshrc $HOME/.zshuery $HOME/.bash_profile $HOME/.tmux.conf $HOME/.vimrc $HOME/.redteam_helpers"
 rsync -avh `echo ${dotfiles}` .
 
 git add --all
